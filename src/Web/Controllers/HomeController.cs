a457cbd101ac71ed86af0205690507b95b70b1e6
Daniel González García <danielgonnet@gmail.com>
2013-09-11 16:29:06 +0200

    first commit: empty MVC4 application

---
+++ src/Web/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace NMoneys.Web.Controllers
+{
+	public class HomeController : Controller
+	{
+		 public ActionResult Index()
+		 {
+			 ViewBag.Header = "nMoneys";
+			 return View();
+		 }
+	}
+}
---
+++ src/Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NMoneys.Web.MvcApplication" Language="C#" %>
