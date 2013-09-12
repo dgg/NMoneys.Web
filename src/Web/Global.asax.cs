5eb0a2cdcbf001d750d6db316722152870a736f2
Daniel González García <danielgonnet@gmail.com>
2013-09-12 14:03:44 +0200

    slimmed down landing page and aded optimization of view engine

--- src/Web/Global.asax.cs
+++ src/Web/Global.asax.cs
@@ -1,19 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Http;
+﻿using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 using NMoneys.Web.App_Start;
+using NMoneys.Web.Views;
 
 namespace NMoneys.Web
 {
 	// Note: For instructions on enabling IIS6 or IIS7 classic mode, 
 	// visit http://go.microsoft.com/?LinkId=9394801
 
-	public class MvcApplication : System.Web.HttpApplication
+	public class MvcApplication : HttpApplication
 	{
 		protected void Application_Start()
 		{
@@ -21,7 +18,15 @@ namespace NMoneys.Web
 
 			FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
 			RouteConfig.RegisterRoutes(RouteTable.Routes);
+
 			BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+			Currency.InitializeAllCurrencies();
+
+			ViewEngines.Engines.Clear();
+			var razor = new RazorViewEngine();
+			razor.ViewLocationCache = new TwoLevelViewCache(razor.ViewLocationCache);
+			ViewEngines.Engines.Add(razor);
 		}
 	}
 }
---
+++ src/Web/Views/TwoLevelViewCache.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NMoneys.Web.Views
+{
+	public class TwoLevelViewCache : IViewLocationCache
+	{
+		private readonly IViewLocationCache _secondLevelCache;
+
+		public TwoLevelViewCache(IViewLocationCache secondLevelCache)
+		{
+			_secondLevelCache = secondLevelCache;
+		}
+
+		private readonly static object _key = new object();
+		private static IDictionary<string, string> getRequestCache(HttpContextBase httpContext)
+		{
+			var d = httpContext.Items[_key] as IDictionary<string, string>;
+			if (d == null)
+			{
+				d = new Dictionary<string, string>();
+				httpContext.Items[_key] = d;
+			}
+			return d;
+		}
+
+		public string GetViewLocation(HttpContextBase httpContext, string key)
+		{
+			var firstLevelCache = getRequestCache(httpContext);
+			string location;
+			if (!firstLevelCache.TryGetValue(key, out location))
+			{
+				location = _secondLevelCache.GetViewLocation(httpContext, key);
+				firstLevelCache[key] = location;
+			}
+			return location;
+		}
+
+		public void InsertViewLocation(HttpContextBase httpContext, string key, string virtualPath)
+		{
+			_secondLevelCache.InsertViewLocation(httpContext, key, virtualPath);
+		}
+	}
+}
