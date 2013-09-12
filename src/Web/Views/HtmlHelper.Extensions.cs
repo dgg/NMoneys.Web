f7bdbb13197f5969c4581868943dc705e27c311d
Daniel González García <danielgonnet@gmail.com>
2013-09-12 13:00:21 +0200

    move html helpers to use HtmlTags

--- src/Web/Views/HtmlHelper.Extensions.cs
+++ src/Web/Views/HtmlHelper.Extensions.cs
@@ -1,34 +1,30 @@
 ﻿using System;
 using System.Linq.Expressions;
-using System.Web;
 using System.Web.Mvc;
 using System.Web.Mvc.Html;
 using System.Web.Routing;
 using System.Web.UI;
+using HtmlTags;
 
 namespace NMoneys.Web.Views
 {
 	public static class HtmlHelperExtensions
 	{
-		 public static IHtmlString NavEntry<TController>(this HtmlHelper helper, Expression<Action<TController>> action, string linkText, string classIfCurrent = "active") where TController : Controller
+		public static HtmlTag NavEntry<TController>(this HtmlHelper helper, Expression<Action<TController>> action, string linkText, string classIfCurrent = "active") where TController : Controller
 		 {
 			 RouteValueDictionary values = Microsoft.Web.Mvc.Internal.ExpressionHelper.GetRouteValuesFromExpression(action);
 
-			 TagBuilder li = HtmlTextWriterTag.Li.asTag(tb => 
-				 tb.InnerHtml = helper.RouteLink(linkText, values).ToHtmlString());
-			 if (isCurrentAction(helper, values))
-			 {
-				 li.AddCssClass(classIfCurrent);
-			 }
+			 HtmlTag li = HtmlTextWriterTag.Li.asTag()
+				 .AddClassIf(isCurrentAction(helper, values), classIfCurrent)
+				 .AppendHtml(
+					helper.RouteLink(linkText, values).ToHtmlString());
 
-			 return new HtmlString(li.ToString(TagRenderMode.Normal));
+			 return li;
 		 }
 
-		private static TagBuilder asTag(this HtmlTextWriterTag tag, Action<TagBuilder> building = null)
+		private static HtmlTag asTag(this HtmlTextWriterTag tag)
 		{
-			var builder = new TagBuilder(tag.ToString().ToLower());
-			if (building != null) building(builder);
-			return builder;
+			return new HtmlTag(tag.ToString());
 		}
 
 		enum Key
@@ -61,25 +57,29 @@ namespace NMoneys.Web.Views
 			return values[key.ToString()].ToString();
 		}
 
-		public static IHtmlString IconNavEntry<TController>(this HtmlHelper helper, Expression<Action<TController>> action, string iconClass, string classIfCurrent = "active") where TController : Controller
+		public static HtmlTag IconNavEntry<TController>(this HtmlHelper helper, Expression<Action<TController>> action, string iconClass, string classIfCurrent = "active") where TController : Controller
 		{
 			RouteValueDictionary values = Microsoft.Web.Mvc.Internal.ExpressionHelper.GetRouteValuesFromExpression(action);
 			
 			string url = UrlHelper.GenerateUrl(null, null, null, null, null, null, values, helper.RouteCollection, helper.ViewContext.RequestContext, false);
-			TagBuilder anchor = HtmlTextWriterTag.A.asTag(tb => tb.MergeAttribute("href", url));
 
-			TagBuilder span = HtmlTextWriterTag.Span.asTag(tb => tb.AddCssClass(iconClass));
-			anchor.InnerHtml = span.ToString(TagRenderMode.Normal);
+			var li = new HtmlTag("li")
+				.AddClassIf(isCurrentAction(helper, values), classIfCurrent)
+				.Append(
+					new LinkTag(string.Empty, url).Append(
+						new HtmlTag("span").AddClass(iconClass)
+					)
+				);
+			return li;
+		}
 
-			TagBuilder li = HtmlTextWriterTag.Li.asTag(tb =>
-				tb.InnerHtml = anchor.ToString(TagRenderMode.Normal));
-
-			if (isCurrentAction(helper, values))
+		public static HtmlTag AddClassIf(this HtmlTag tag, bool condition, string className)
+		{
+			if (condition)
 			{
-				li.AddCssClass(classIfCurrent);
+				tag.AddClass(className);
 			}
-
-			return new HtmlString(li.ToString(TagRenderMode.Normal));
+			return tag;
 		}
 	}
 }
