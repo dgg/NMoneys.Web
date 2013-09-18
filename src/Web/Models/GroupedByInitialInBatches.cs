7eca756e81804b401ea4d702c40403fabd5bf6b4
Daniel González García <danielgonnet@gmail.com>
2013-09-18 14:41:32 +0200

    solved sorting issue

--- src/Web/Models/GroupedByInitialInBatches.cs
+++ src/Web/Models/GroupedByInitialInBatches.cs
@@ -17,7 +17,7 @@ namespace NMoneys.Web.Models
 		public static IEnumerable<GroupedByInitialInBatches> Collection(IEnumerable<Snapshot> snapshots, Func<Snapshot, string> text, int batchSize)
 		{
 			return snapshots.GroupBy(i => text(i)[0])
-				.OrderBy(StringComparer.OrdinalIgnoreCase.Equals)
+				.OrderBy(g => g.Key, Comparer<char>.Default)
 				.Select(g => new GroupedByInitialInBatches(g, batchSize));
 		}
 	}
