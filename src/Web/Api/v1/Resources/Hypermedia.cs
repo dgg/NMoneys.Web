1ff210e7883c8d4d67c62b35b0687018316f36a5
Daniel González García <danielgonnet@gmail.com>
2013-11-17 16:49:03 +0100

    try to fix problem with casing of files

--- src/Web/Api/v1/Resources/Hypermedia.cs
+++ src/Web/Api/v1/Resources/Hypermedia.cs
@@ -7,7 +7,7 @@ using ServiceStack.ServiceInterface;
 
 namespace NMoneys.Web.Api.v1.Resources
 {
-	public class Hypermedia : Service, IOptions<root>, IOptions<currencies>, IOptions<currency>, IOptions<format>
+	/*public class Hypermedia : Service, IOptions<root>, IOptions<currencies>, IOptions<currency>, IOptions<format>
 	{
 		public object Options(root request)
 		{
@@ -77,4 +77,55 @@ namespace NMoneys.Web.Api.v1.Resources
 			return response;
 		}
 	}
+	 [Route("/v1/currencies", "OPTIONS", Summary = "List current supported currencies.", Notes = "Obsolete currencies are not returned.")]
+	[Api("List current supported currencies.")]
+	public class currencies : IReturn<currenciesResponse> { }
+
+	public class currenciesResponse : IReturn<currenciesResponse>
+	{
+		public Link[] _links { get; set; }
+	}
+	 * 
+	 * [Route("/v1/currencies/{isoCode}", "OPTIONS", Summary = "Get detailed information about a currency.")]
+	[Api("Get detailed information about a currency.")]
+	public class currency : IReturn<currencyResponse>
+	{
+		[ApiMember(IsRequired = true, ParameterType = "path", Description = "Three-letter ISO code of the currency to return.")]
+		public CurrencyIsoCode IsoCode { get; set; }
+	}
+
+	public class currencyResponse
+	{
+		public Link[] _links { get; set; }
+	}
+	 * 
+	 * [Route("/v1/currencies/{isoCode}/format", "OPTIONS", Summary = "Allows formatting a monetary amount according to its currency.")]
+	[Route("/v1/currencies/{isoCode}/format/{amount}", "OPTIONS", Summary = "Allows formatting a monetary amount according to its currency.")]
+	[Api("Allows formatting a monetary amount according to its currency.")]
+	public class format : IReturn<formatResponse>
+	{
+		[ApiMember(IsRequired = true, ParameterType = "path, body", Verb = "GET, POST",
+			Description = "Three-letter ISO code of the currency to use for formatting.")]
+		public CurrencyIsoCode IsoCode { get; set; }
+
+		[ApiMember(IsRequired = true, ParameterType = "path, body", Verb = "GET, POST",
+			Description = "Amount of the monetary quantity to be formatted.")]
+		public decimal Amount { get; set; }
+	}
+
+	public class formatResponse
+	{
+		public Link[] _links { get; set; }
+	}
+	 * 
+	 * [Route("/v1", "OPTIONS", Summary = "List operations available through the API.")]
+	[Api("List operations available through the API.")]
+	public class root : IReturn<rootResponse> { }
+
+	public class rootResponse
+	{
+		public Link[] _links { get; set; }
+	}
+	 
+	 */
 }
